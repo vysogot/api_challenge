78dc94709ef13825d0abddd5b0f2586aefb36bad
Jakub Godawa <jakub.godawa@gmail.com>
2024-07-15 21:28:10 +0200

    remove files

--- db/migrate/20240209123244_add_columns_to_bikes.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddColumnsToBikes < ActiveRecord::Migration[7.1]
-  def change
-    add_column :bikes, :latitude, :float
-    add_column :bikes, :longitude, :float
-  end
-end
